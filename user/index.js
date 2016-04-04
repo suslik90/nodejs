54f48281731ca7bced4546e7c82582074e336368
suslik90 <romansmekalov@gmail.com>
2016-04-04 22:51:52 +0300

    Раздел 5. Модули для NodeJS

---
+++ user/index.js
@@ -0,0 +1,16 @@
+//exports - для экспорта данных между модулями
+//global -
+
+var phrases = require("./ru");
+
+function User(name){
+    this.name = name;
+}
+
+User.prototype.hello = function(who){
+    console.log(phrases.Hello+  ", "+who.name);
+};
+
+console.log("user.js is required!");
+
+exports.User = User;
